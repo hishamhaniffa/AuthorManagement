9d32b7373ada063508aa4a1c6e353b49520a9077
Hisham Haniffa <hishamhaniffa4@gmail.com>
2016-02-04 15:58:55 +0530

    Updated routes with react-router and added Link to normalize routing:

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,7 +1,8 @@
 'use strict';
 
 var React = require('react');
-
+var Router = require('react-router');
+var Link = Router.Link;
 var Home = React.createClass({
 	render: function() {
 		return (
@@ -9,6 +10,7 @@ var Home = React.createClass({
 				<div className="container">
 					<h1>Pluralsight Adminstration</h1>
 					<p>React, React Router, and Flux for ultra responsive web apps.</p>
+					<Link to="about" className="btn btn-primary btn-lg">Learn more</Link>
 				</div>
             </div>
 		);
--- src/main.js
+++ src/main.js
@@ -1,40 +1,10 @@
-$ = jQuery = require('jquery');
+'use strict';
 
 var React = require('react');
-var Home = require('./components/homepage');
-var About = require('./components/about/aboutpage');
-var Header = require('./components/common/header');
-var Authors = require('./components/authors/authorpage');
-
-(function(win){
-	"use strict";
-	var App = React.createClass({
-		render: function() {
-			var Child;
-
-			switch(this.props.route) {
-				case 'about': Child = About; break;
-				case 'authors': Child = Authors; break;
-				default: Child = Home;
-			}
-
-			return (
-				<div>
-					<Header />
-					<Child />
-				</div>
-			);
-		}
-	});
-
-	function render() {
-		var route = win.location.hash.substr(1);
-		React.render(<App route={route} />, document.getElementById('app'));
-	}
-
-	win.addEventListener('hashchange', render);
-	render();
-})(window);
+var Router = require('react-router');
+var routes = require('./routes');
 
 
-// React.render(<Home />, document.getElementById('app'));
+Router.run(routes, function(Handler) {
+	React.render(<Handler />, document.getElementById('app'));
+});
