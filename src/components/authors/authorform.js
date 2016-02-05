03d3d290762a522fa86600eee238551b06a2e86b
Hisham Haniffa <hishamhaniffa4@gmail.com>
2016-02-05 13:35:46 +0530

    Add textinput component to have abstraction with input types

--- src/components/authors/authorform.js
+++ src/components/authors/authorform.js
@@ -8,16 +8,6 @@ var AuthorForm = React.createClass({
 			<form className="row col-sm-4 col-sm-offset-3">
 				<h1>Manage Author</h1>
 				<div className="form-group">
-					<label htmlFor="firstName" className="label-control">First Name</label>
-					<input type="text" 
-							className="form-control" 
-							placeholder="First Name" 
-							name="firstName" 
-							ref="firstName" 
-							value={this.props.author.firstName} 
-							onChange={this.props.onChange} />
-				</div>
-				<div className="form-group">
 					<label htmlFor="lastName" className="label-control">Last Name</label>
 					<input type="text" 
 							className="form-control" 
--- src/components/common/textinput.js
+++ src/components/common/textinput.js
@@ -3,7 +3,33 @@
 var React = require('react');
 
 var Input = React.createClass({
+	propTypes: {
+		name: React.PropTypes.string.isRequired,
+		label: React.PropTypes.string.isRequired,
+		onChange: React.PropTypes.func.isRequired,
+		placeholder: React.PropTypes.string,
+		value: React.PropTypes.string,
+		error: React.PropTypes.string
+	},
 	render: function() {
-
+		var wrapperClass = 'form-group';
+		if(this.props.error && this.props.error.length >0) {
+			wrapperClass += " " + 'has-error';
+		}
+		<div className={wrapperClass}>
+			<label htmlFor={this.props.name} className="label-control">{this.props.label}</label>
+			<div className="field">
+				<input type="text"
+						className="form-control"
+						placeholder={this.props.placeholder}
+						name={this.props.name}
+						ref={this.props.name}
+						value={this.props.value}
+						onChange={this.props.onChange} />
+				<div className="input">{this.props.error}</div>
+			</div>
+		</div>
 	}
-})
+});
+
+module.exports = Input;
