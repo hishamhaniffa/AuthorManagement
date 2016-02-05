29795a2294f47fee945d109521dec1594fddea99
Hisham Haniffa <hishamhaniffa4@gmail.com>
2016-02-05 13:39:18 +0530

    Add input component to authorform component

--- src/components/authors/authorform.js
+++ src/components/authors/authorform.js
@@ -1,22 +1,25 @@
 'use strict';
 
 var React = require('react');
+var Input = require('../common/textInput');
 
 var AuthorForm = React.createClass({
 	render: function() {
 		return (
 			<form className="row col-sm-4 col-sm-offset-3">
 				<h1>Manage Author</h1>
-				<div className="form-group">
-					<label htmlFor="lastName" className="label-control">Last Name</label>
-					<input type="text" 
-							className="form-control" 
-							placeholder="Last Name" 
-							name="lastName" 
-							ref="lastName" 
-							value={this.props.author.lastName} 
-							onChange={this.props.onChange}/>
-				</div>
+				<Input
+					name="firstName"
+					label="First Name"
+					value={this.props.author.firstName}
+					onChange={this.props.onChange} />
+
+				<Input
+					name="lastName"
+					label="Last Name"
+					value={this.props.author.lastName}
+					onChange={this.props.onChange} />
+					
 				<input type="submit" value="Save" className="btn btn-default" />
 			</form>
 		);
